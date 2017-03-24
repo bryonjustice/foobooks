7dbca417ca04db15d2934ddcd7b09508f84cefad
Bryon Justice <bryon_justice@yahoo.com>
2017-03-23 23:02:47 -0400

    adding search

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,21 @@
 */
 Route::get('/books', 'BookController@index');
 
+Route::get('/books/new', 'BookController@createNewBook');
+Route::post('/books/new', 'BookController@storeNewBook');
+
 Route::get('/books/{title?}', 'BookController@show');
 
+Route::get('/search', 'BookController@search');
 
+/**
+* Log viewer
+* (only accessible locally)
+*/
 if(config('app.env') == 'local') {
     Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 }
+
 
 /**
 * Practice
